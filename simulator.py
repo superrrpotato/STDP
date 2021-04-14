8fdf6259e9d6167507780f48d9ecf72deac32ad7
Yukun Yang <yukunyang@ucsb.edu>
2021-04-14 13:02:54 +0800

    add STDP_sum

--- simulator.py
+++ simulator.py
@@ -28,7 +28,7 @@ class recurrent_network:
         self.weight_matrix = np.random.normal(size=(self.neuron_num, self.neuron_num)).astype(self.dtype)*0.2
         row, col = np.diag_indices_from(self.weight_matrix)
         self.weight_matrix[row,col] = -self.threshold
-        self.input_weight = np.random.normal(size=(self.neuron_num)).astype(self.dtype)*0.2
+        self.input_weight = np.random.normal(size=(self.neuron_num)).astype(self.dtype)*0.1
         self.membrane_potentials = np.zeros((self.neuron_num, self.time_steps), dtype=self.dtype)
         self.firing_rate = np.zeros((self.neuron_num, self.time_steps), dtype=self.dtype)
         self.spike_train = np.zeros((self.neuron_num, self.time_steps), dtype=np.int8)
@@ -42,7 +42,7 @@ class recurrent_network:
         self.mem_decay = 1 - 1/self.tau_mem
         self.tau_psc = init_params['tau_psc']
         self.psc_decay = 1 - 1/self.tau_psc
-        self.fsize = 12
+        self.fsize = 13
     def plot(self):
         
         size_factor = 0.12
@@ -87,15 +87,29 @@ class recurrent_network:
         self.weight_update = np.clip(self.weight_update, 0, 2)
         self.weight_matrix = self.weight_matrix * self.weight_update 
         energy = np.linalg.norm(self.weight_matrix)
-        factor = 10/energy
+        factor = 8/energy
         self.weight_matrix *= factor
-#         self.weight_matrix = self.weight_matrix / np.max(np.abs(self.weight_matrix[~np.eye(self.weight_matrix.shape[0],dtype=bool)]))
+        row, col = np.diag_indices_from(self.weight_matrix)
+        self.weight_matrix[row,col] = -self.threshold
+    def STDP_sum(self):
+        self.weight_update = np.ones(shape=(self.neuron_num, self.neuron_num),dtype=np.float32)
+        learning_rate = self.lr
+        window_length = self.window_size
+        window_t = np.arange(-window_length,window_length+1,1)
+        STDP_func = 0.42*np.exp(-window_t**2/(10))*(np.sign(window_t)+0.5)
+        relative_effect = np.array([np.convolve(a.spike_train[i], STDP_func) for i in range(a.neuron_num)])
+        updates = learning_rate * np.matmul(relative_effect[:,window_length:-window_length],self.spike_train.T)/self.neuron_num
+        self.weight_matrix += updates
+        self.weight_update = updates
+        row, col = np.diag_indices_from(self.weight_update)
+        self.weight_update[row,col] = 0.
+        energy = np.linalg.norm(self.weight_matrix)
+        factor = 8/energy
+        self.weight_matrix *= factor
         row, col = np.diag_indices_from(self.weight_matrix)
         self.weight_matrix[row,col] = -self.threshold
     def STDP_plot(self):
-        
         plt.figure(figsize=(12,3))
-#         plt.clf()
         plt.subplot(1,3,1)
         plt.imshow(self.weight_matrix)
         plt.title('new weight matrix', fontsize = self.fsize)
@@ -107,28 +121,27 @@ class recurrent_network:
         plt.subplot(1,3,3)
         plt.hist(self.weight_matrix[~np.eye(self.weight_matrix.shape[0],dtype=bool)].ravel(),bins=50)
         plt.title('weight distribution', fontsize = self.fsize)
-#         plt.pause(0.05)
     def auto_plot(self):
         plt.clf()
-        plt.subplot(2,4,1)
+        plt.subplot(3,4,1)
         plt.imshow(self.weight_matrix)
         plt.title('new weight matrix', fontsize = self.fsize)
         plt.colorbar()
-        plt.subplot(2,4,2)
+        plt.subplot(3,4,2)
         plt.imshow(self.weight_update)
         plt.title('weight updating signal', fontsize = self.fsize)
         plt.colorbar()
-        plt.subplot(2,4,3)
+        plt.subplot(3,4,3)
         plt.hist(self.weight_matrix[~np.eye(self.weight_matrix.shape[0],dtype=bool)].ravel(),bins=50)
         plt.title('weight distribution', fontsize = self.fsize)
-        plt.subplot(2,4,4)
+        plt.subplot(3,4,4)
         self.graph_plot()
         size_factor = 0.12
         total_plots = 5
-        name_list = ['psc','spike_train','membrane_potentials','firing_rate','input']
-        value_list = [self.psc,self.spike_train,self.membrane_potentials,np.clip(self.firing_rate,0,1),self.input]
-        for i in [0]:
-            plt.subplot(2,1,2+i)
+        name_list = ['membrane_potentials','postsynaptic_current','spike_train','firing_rate','input']
+        value_list = [self.membrane_potentials,self.psc,self.spike_train,np.clip(self.firing_rate,0,1),self.input]
+        for i in [0,1]:
+            plt.subplot(3,1,2+i)
             plt.imshow(value_list[i])
             plt.title(name_list[i], fontsize = self.fsize)
         plt.pause(0.05)
@@ -137,17 +150,33 @@ class recurrent_network:
         G.add_nodes_from(range(self.neuron_num))
         for i in range(self.neuron_num):
             for j in range(self.neuron_num):
-                c = 'tab:green' if self.weight_matrix[i,j]>0 else 'tab:orange'
+                c = 'gold' if self.weight_matrix[i,j]>0 else 'navy'
                 G.add_edge(i, j, color = c, weight = self.weight_matrix[i,j])
         weights = [G[u][v]['weight'] for u,v in G.edges]
         colors = [G[u][v]['color'] for u,v in G.edges]
         options = {
-            'node_color': 'tab:blue',
+            'node_color': 'mediumseagreen',
             'width': np.abs(weights),
             'edge_color': colors,
-            'node_size': 30
+            'node_size': 40,
         }
         nx.draw_circular(G, **options)
+    def fast_auto_plot(self):
+        plt.clf()
+        plt.subplot(1,4,1)
+        plt.imshow(self.weight_matrix)
+        plt.title('new weight matrix', fontsize = self.fsize)
+        plt.colorbar()
+        plt.subplot(1,4,2)
+        plt.imshow(self.weight_update)
+        plt.title('weight updating signal', fontsize = self.fsize)
+        plt.colorbar()
+        plt.subplot(1,4,3)
+        plt.hist(self.weight_matrix[~np.eye(self.weight_matrix.shape[0],dtype=bool)].ravel(),bins=50)
+        plt.title('weight distribution', fontsize = self.fsize)
+        plt.subplot(1,4,4)
+        self.graph_plot()
+        plt.pause(0.05)
         
 plt.figure(figsize=(12,10))
 # %matplotlib inline
