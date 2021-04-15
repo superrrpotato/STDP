d1b84cdfb6228309cf4e957402c308fe011d7e52
Yukun Yang <yukunyang@ucsb.edu>
2021-04-15 16:11:16 +0800

    no log record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from network_parser import parse
 import logging
-
+import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-config', action='store', dest='config',\
