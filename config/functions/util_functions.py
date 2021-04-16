1995b79469343b82ad7f45902ddbb4b51eedb7cc
Yukun Yang <yukunyang@ucsb.edu>
2021-04-16 22:41:44 +0800

    add psp function

---
+++ config/functions/util_functions.py
@@ -0,0 +1,14 @@
+import torch
+def psp(inputs, tau_psc):
+    shape = inputs.shape
+    # n_steps = network_config['n_steps']
+    # tau_s = network_config['tau_s']
+
+    syn = torch.zeros(shape[0], dtype=glv.dtype, device=glv.device)
+    syns = torch.zeros(shape, dtype=glv.dtype, device=glv.device)
+
+    for t in range(shape[1]):
+        syn = syn * ( 1 - 1 / tau_psc) + (1 / tau_psc) * inputs[:, t]
+        syns[:, t] = syn
+
+    return syns
